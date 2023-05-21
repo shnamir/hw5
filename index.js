6d9044ef7c57a485feb2d7c4ff825a427e843325
amir <shiodiyev.amir123@example.com>
2023-05-21 21:53:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//№1
+let number  = prompt('Напишите число')
+
+if(number % 2 == 0){
+    alert('Четное')
+}else{
+    alert('Не четное')
+}
+
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/shnamir/hw5.git
+git push -u origin main
